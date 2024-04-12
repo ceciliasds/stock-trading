d8f99871f5095b165177dbca3aeb470304c10ae3
avitot16 <144504779+avitot16@users.noreply.github.com>
2024-04-12 20:19:20 +0800

    Changcoco, Avigael

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -98,5 +98,170 @@ class _HomePageState extends State<HomePage> {
     return 0.0; // Return a double value
   }
 
-//override
+//override 
+ 
+ @override
+Widget build(BuildContext context) {
+  return Scaffold(
+    backgroundColor: Color.fromARGB(255, 26, 67, 113),
+    appBar: AppBar(
+      backgroundColor: Color.fromARGB(255, 48, 67, 83),
+      title: SizedBox(
+        height: 35, // Adjust the height here
+        child: TextField(
+          controller: _searchController,
+          decoration: InputDecoration(
+            hintText: 'Enter company symbol...',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30.0), // Adjust the roundness here
+              borderSide: BorderSide(
+                color: Colors.grey, // Adjust the border color here
+              ),
+            ),
+            filled: true,
+            fillColor: Colors.white,
+            contentPadding: EdgeInsets.fromLTRB(12, 4, 8, 4), // Adjust the padding here
+            hintStyle: TextStyle(fontSize: 14), // Adjust the font size here
+          ),
+          style: TextStyle(fontSize: 14), // Adjust the font size here
+          onSubmitted: (value) {
+            if (value.isNotEmpty) {
+              fetchStockData(value);
+              setState(() {
+                symbol = value; // Update the symbol variable
+              });
+            }
+          },
+        ),
+      ),
+      actions: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(right: 8.0), // Add margin to the right
+          child: SizedBox(
+            width: 40, // Adjust the width here
+            height: 40, // Adjust the height here
+            child: CircleAvatar(
+              backgroundColor: Colors.white, // Set background color to white
+              child: IconButton(
+                icon: Icon(Icons.person),
+                color: Colors.black, // Set icon color to black
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ProfilePage(
+                        initialMoney: 1000, // Provide the initial money here
+                        boughtStocks: boughtStocks,
+                        maxShares: maxShares,
+                        
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+      ],
+      automaticallyImplyLeading: false, // Disable back button
+    ),
+    body: Padding(
+  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0), // Adjust padding as needed
+  child: symbol == null || symbol.isEmpty // Modify this line
+      ? Center(
+          // Display a placeholder if symbol is null
+        )
+      : SizedBox(
+          width: double.infinity, // Set the width to match the parent width
+          height: 300, // Set the desired height
+          child: Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start, // Align children to the start (left side)
+              children: <Widget>[
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween, // Distribute children evenly along the row
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start, // Align children to the start (left side)
+                      children: [
+                       GestureDetector(
+  onTap: () {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => CompanyPage(symbol: symbol),
+      ),
+    );
+  },
+  child: Text(
+    '$symbol',
+    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+  ),
+),
+
+                        SizedBox(height: 5), // Add some space between the text
+                        FutureBuilder(
+                          future: fetchCurrentClosePrice(symbol),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState == ConnectionState.waiting) {
+                              return CircularProgressIndicator();
+                            } else if (snapshot.hasError) {
+                              return Text('Error: ${snapshot.error}');
+                            } else {
+                              return Text(
+                                '${snapshot.data != null ? snapshot.data : 'N/A'}',
+                                style: TextStyle(fontSize: 16),
+                              );
+                            }
+                          },
+                        ),
+                      ],
+                    ),
+                    ElevatedButton(
+  onPressed: () {
+    _openTradeModal(symbol); // Pass symbol as an argument
+  },
+  child: Text('Trade'),
+),
+
+                  ],
+                ),
+                SizedBox(height: 10),
+                Expanded(
+                  child: Center(
+                    child: stockData.isNotEmpty
+                        ? SfCartesianChart(
+                            primaryXAxis: CategoryAxis(),
+                            series: <CandleSeries<Map<String, dynamic>, String>>[
+                              CandleSeries<Map<String, dynamic>, String>(
+                                dataSource: stockData,
+                                xValueMapper: (Map<String, dynamic> data, _) => data['priceDate'],
+                                lowValueMapper: (Map<String, dynamic> data, _) => data['low'],
+                                highValueMapper: (Map<String, dynamic> data, _) => data['high'],
+                                openValueMapper: (Map<String, dynamic> data, _) => data['open'],
+                                closeValueMapper: (Map<String, dynamic> data, _) => data['close'],
+                              )
+                            ],
+                          )
+                        : SizedBox(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+),
+
+  );
+
+
+
+
+  }
 }
